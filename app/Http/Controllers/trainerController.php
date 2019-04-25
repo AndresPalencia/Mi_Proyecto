d27cc661f660088b7b9f50b151f9dd277ffd1353
AndresPalencia <anpa.ank@gmail.com>
2019-04-25 18:17:10 -0400

    Video 21+22

--- app/Http/Controllers/trainerController.php
+++ app/Http/Controllers/trainerController.php
@@ -60,9 +60,8 @@ class trainerController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Trainer $trainer)
     {
-        $trainer = Trainer::find($id);
         return view('trainers.show', compact('trainer'));
     }
 
--- resources/views/trainers/index.blade.php
+++ resources/views/trainers/index.blade.php
@@ -12,7 +12,7 @@
  			<div class="card-body">
     			<h5 class="card-title">{{$trainer->name}}</h5>
     			<p class="card-text">{{$trainer->description}}</p>
-    			<a href="/trainers/{{$trainer->id}}" class="btn btn-primary">Mostrar</a>
+    			<a href="/trainers/{{$trainer->slug}}" class="btn btn-primary">Mostrar</a>
   			</div>
   		</div>
   	</div>	
