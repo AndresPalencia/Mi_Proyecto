98ef6a0b22eb7fe5893e79d0b65cebd62583f3cd
AndresPalencia <anpa.ank@gmail.com>
2019-05-01 17:30:36 -0400

    Video 25

--- resources/views/trainers/create.blade.php
+++ resources/views/trainers/create.blade.php
@@ -4,29 +4,10 @@
 
 @section('content')
 
-<br>
 {!! Form::open(['route' => 'trainers.store', 'method' => 'POST', 'files' => true ]) !!}
 
-	<div class="form-group">
-		{!! Form::label('name', 'Nombre') !!}
-		{!! Form::text('name', null, ['class' => 'form-control']) !!}
-	</div>
-	
-	<div class="form-group">
-		{!! Form::label('slug', 'Slug') !!}
-		{!! Form::text('slug', null, ['class' => 'form-control']) !!}
-	</div>
-<br>
-	<div class="form-group">
-		{!! Form::label('avatar', 'Avatar') !!}
-		{!! Form::file('avatar') !!}
-	</div>
-<br>
-	<div class="form-group">
-		{!! Form::label('description', 'Descripcion') !!}
-		{!! Form::text('description', null, ['class' => 'form-control']) !!}
-	</div>
-<br>
+	@include('trainers.form')
+
 	{!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}	
 {!! Form::close() !!}
 
